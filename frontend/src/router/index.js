e59ca99f808603ea800f13169e8c51382cba64b5
Earrow <earrow.liu@gmail.com>
2020-01-06 17:03:12 +0800

    repair repeat route error

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,11 @@ import store from '@/store'
 
 Vue.use(VueRouter)
 
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
+
 const routes = [
   {
     path: '/',
